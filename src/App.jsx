af13556d274a9c981a9a6b1b176ff732aa0cd9a5
André Ferreira <andreferreira2302145@gmail.com>
2022-09-19 12:05:51 +0100

    Render informação de clientes

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Component } from "react";
 import MainContent from "./MainContent";
 import NavBar from "./NavBar";
 
+//App é a raiz, NavBar e MainContent são child componentes
 class App extends Component {
   render() {
     return (
